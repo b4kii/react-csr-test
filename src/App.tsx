8663f84ad8dc4e3f06e924a13228942a77ba2b88
b4kii <dami4nhalibart@gmail.com>
2025-03-06 22:11:54 +0100

    testing react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 
-const API_URL = "https://randomuser.me/api/";
-
-async function fetchUser() {
-  try {
-    const res = await fetch(API_URL);
-
-    const data = await res.json();
-
-    return data.results[0];
-  } catch (err) {
-    console.error(err);
-    return null;
-  }
-}
 
 function App() {
-  const { data: user, isLoading } = useQuery({
-    queryFn: () => fetchUser(),
-    queryKey: ["user"],
-  });
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <div className="p-2 py-4">
       <h1>{user?.name.first}</h1>
--- src/routes/users/$userId.tsx
+++ src/routes/users/$userId.tsx
@@ -1,10 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { z } from "zod";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useQuery } from "@tanstack/react-query";
 
 const userSearchSchema = z.object({
-  q: z.string().min(2, {message: "Search key is too short"}),
+  q: z.string().min(2, { message: "Search key is too short" }),
   asc: z.boolean(),
-  timestamp: z.number()
+  timestamp: z.number(),
 });
 
 // type UserSearch = z.infer<typeof userSearchSchema>
@@ -13,23 +16,104 @@ export const Route = createFileRoute("/users/$userId")({
   validateSearch: userSearchSchema,
   loaderDeps: ({ search: { q, asc, timestamp } }) => ({ q, asc, timestamp }),
   component: UserComponent,
-  loader: async ({ deps , params}) => {
-    return `This is data from loader function params: ${params.userId}\nsearch: ${deps.q}, ${deps.asc}, ${deps.timestamp}`
+  loader: async ({ deps, params }) => {
+    return `This is data from loader function params: ${params.userId}\nsearch: ${deps.q}, ${deps.asc}, ${deps.timestamp}`;
   },
   pendingComponent: () => <div>Loading..</div>,
-  errorComponent: () => <div>Error</div>
+  errorComponent: () => <div>Error</div>,
 });
+
+const userLoginFormSchema = z.object({
+  login: z.string().min(2, { message: "Username too short" }),
+  password: z.string(),
+});
+
+type UserLoginForm = z.infer<typeof userLoginFormSchema>;
+
+const API_URL = "https://randomuser.me/api/";
+
+async function fetchUser() {
+  try {
+    const res = await fetch(API_URL);
+
+    const data = await res.json();
+
+    return data.results[0];
+  } catch (err) {
+    console.error(err);
+    return null;
+  }
+}
+
 
 function UserComponent() {
   const data = Route.useLoaderData();
   const { q } = Route.useSearch();
   const { userId } = Route.useParams();
 
+  const { data: user, isLoading, refetch, isFetching} = useQuery({
+    queryFn: () => fetchUser(),
+    queryKey: ["user"],
+    enabled: false
+  });
+
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm<UserLoginForm>({
+    resolver: zodResolver(userLoginFormSchema),
+    defaultValues: {
+      login: "baki"
+    }
+  });
+
+  // const onSubmit: SubmitHandler<UserLoginForm> = async (data) => {
+  //   try {
+  //     await new Promise((resolve) => setTimeout(resolve, 3000));
+  //     throw new Error();
+  //     console.log(data);
+  //   } catch (error) {
+  //     setError("login", { message: "Invalid login" }); // root error = global error
+  //   }
+  // };
+
+  const onSubmit: SubmitHandler<UserLoginForm> = async (data) => {
+      console.log(data);
+  };
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+
   return (
     <div>
       <div>Loader data: {data}</div>
       <div>Path params: {userId}</div>
       <div>Query params: {q}</div>
+
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <input {...register("login")} type="text" placeholder="Login" />
+            {errors.login && <div>{errors.login.message}</div>}
+          </div>
+          <div>
+            <input
+              {...register("password")}
+              type="password"
+              placeholder="Password"
+            />
+          </div>
+          <button>Sign in</button>
+        </form>
+      </div>
+      <button onClick={() => refetch()}>
+        {isFetching ? "Looooading.." : "Fetch some user"}
+      </button>
+      <h1>{user?.name.first}</h1>
     </div>
-  )
+  );
 }
